bb4740a8c7ccf3fb73af4e6ef641fa8b57268384
Fran-Klasic <fklasich@gmail.com>
2026-03-19 19:03:34 +0100

    Update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
+const redirect = sessionStorage.redirect;
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
